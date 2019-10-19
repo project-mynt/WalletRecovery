32799fddbef8a2e8f2dd2f2baf528880761ec4c5
Tri <tri282@gmail.com>
2019-10-19 09:32:15 -0700

    add ravencoin

--- Coin.js
+++ Coin.js
@@ -2,12 +2,23 @@ module.exports = {
 	pigeon : {
 		messagePrefix: `\x19Pigeoncoin Signed Message:\n`,
 	    bip32: {
+	      public: 76067358,
+	      private: 76066276
+	    },
+	    pubKeyHash: 50,
+	    scriptHash: 122,
+	    wif: 128,
+	    coin : "PGN"
+	},
+	raven : {
+		messagePrefix: `\x19Ravencoin Signed Message:\n`,
+	    bip32: {
 	      public: 142057770,
 	      private: 142057770
 	    },
 	    pubKeyHash: 55,
 	    scriptHash: 122,
 	    wif: 128,
-	    coin : "PGN"
+	    coin : "RVN"
 	}
 }
